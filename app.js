d029b050925189eaba9422d5bcaaf654b91fa3b7
LaraPrimo <lprimosich@gmail.com>
2021-08-20 18:13:42 -0300

    script2-lara

--- app.js
+++ app.js
@@ -3,22 +3,26 @@ const app = express();
 const path = require("path");
 
 app.listen(3030, () => {
-    console.log("Se prendió!");
+  console.log("Se prendió!");
 });
 
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "views/index.html"));
+  res.sendFile(path.join(__dirname, "views/index.html"));
 });
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "views/register.html"));
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/register.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/login.html"));
 });
 
 app.get("/producto-detalle", (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/productDetail.html"));
+  res.sendFile(path.join(__dirname, "/views/productDetail.html"));
 });
 
 app.get("/carrito", (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/productCart.html"));
+  res.sendFile(path.join(__dirname, "/views/productCart.html"));
 });
