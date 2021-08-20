e19388a15d2fbbe43e5fa182f07229b20264acdb
Gaston <gislacasares@gmail.com>
2021-08-19 23:36:57 -0300

    Se agreg lo siguiente: app.js se agrego el endpoint de producto-detalle se agrego en package.json el main de app.js se agrego la vista de productDetail

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
-const express = require ("express");
-const app = express ();
-const path = require ("path");
+const express = require("express");
+const app = express();
+const path = require("path");
 
-app.listen (3030, () => {
-    console.log ("Se prendió!");
+app.listen(3030, () => {
+    console.log("Se prendió!");
 });
 
-app.use (express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get ( "/", (req,res) => {
-    res.sendFile (path.join(__dirname, "views/index.html"));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/index.html"));
 });
-app.get ( "/", (req,res) => {
-    res.sendFile (path.join(__dirname, "views/register.html"));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/register.html"));
+});
+
+app.get("/producto-detalle", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/productDetail.html"));
 });
