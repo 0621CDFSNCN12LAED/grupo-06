e9396b957877528b04d9934e75c2afc6033e718c
Gaston Isla Casares <gislacasares@gmail.com>
2021-09-06 18:11:49 -0300

    cambio app.js y procfile

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ const app = express();
 const path = require("path");
 
 app.listen(3030, () => {
-  console.log("Se prendió!");
+    console.log("Se prendió!");
 });
 
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.set("view engine", "ejs");
 
 app.set("views", "./src/views");
 
-const mainRouter = require("./routes/main-routes");
+const mainRouter = require("./src/routes/main-routes");
 
 app.use("/", mainRouter);
