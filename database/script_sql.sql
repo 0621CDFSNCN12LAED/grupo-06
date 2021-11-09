757a48ff77d133c4f0ce16d76e0d25022f0b40f6
Gaston Isla Casares <gislacasares@gmail.com>
2021-11-09 11:48:09 -0300

    se agrego el DER y el script de creacion de la DB

--- database/script_sql.sql
+++ database/script_sql.sql
@@ -1,3 +1,5 @@
+
+DROP DATABASE tpo_integrador_digitallab;
 CREATE DATABASE tpo_integrador_digitallab;
 
 USE tpo_integrador_digitallab;
@@ -14,9 +16,55 @@ CREATE TABLE Pacientes(
 	contrasenia VARCHAR(100) NOT NULL,
 	terminos TINYINT NOT NULL,
 	PRIMARY KEY(id)
-),
+);
+
+CREATE TABLE Ubicaciones(
+	id INT NOT NULL AUTO_INCREMENT,
+	descripcion VARCHAR(100) NOT NULL,
+	estado TINYINT NOT NULL,
+    PRIMARY KEY(id)
+);
+
 
 CREATE TABLE Estudios(
+	id INT NOT NULL AUTO_INCREMENT,
 	titulo VARCHAR(150) NOT NULL,
+	descripcion TEXT NOT NULL,
+	antes VARCHAR(255) NOT NULL,
+	estudio_ubicacion_id INT NOT NULL,
+	precio FLOAT NOT NULL,
+	img VARCHAR(255) NULL,
+	fecha_creacion DATE NOT NULL,
+	fecha_modificacion DATE NOT NULL,
+	estado TINYINT NOT NULL,
+	PRIMARY KEY(id)
+	/*FOREIGN KEY (estudio_ubicacion_id) REFERENCES Estudio_Ubicacion(id)*/
+);
 
-),
+CREATE TABLE Estudio_Ubicacion(
+	id INT NOT NULL AUTO_INCREMENT,
+	id_estudio INT NOT NULL,
+	id_ubicacion INT NOT NULL,
+	estado TINYINT NOT NULL,
+    PRIMARY KEY(id),
+    --FOREIGN KEY (id_ubicacion) REFERENCES Ubicaciones(id),
+    FOREIGN KEY (id_estudio) REFERENCES Estudios(id)
+);
+
+CREATE TABLE Paciente_Estudio(
+	id INT NOT NULL AUTO_INCREMENT,
+	id_estudio INT NOT NULL,
+	id_paciente INT NOT NULL,    
+    importe FLOAT NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY (id_estudio) REFERENCES Estudios(id),
+    FOREIGN KEY (id_paciente) REFERENCES Pacientes(id)
+);
+
+ALTER TABLE Estudios
+ADD CONSTRAINT FK_estudio_ubicacion
+    FOREIGN KEY (estudio_ubicacion_id) REFERENCES Estudio_Ubicacion(id);
+
+ALTER TABLE Estudio_Ubicacion
+ADD CONSTRAINT FK_ubicacion
+    FOREIGN KEY (id_ubicacion) REFERENCES Ubicaciones(id);
