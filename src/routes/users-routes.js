b760b63359bde56f5b6da07ead80bc570b4409b4
Gaston Isla Casares <gislacasares@gmail.com>
2021-12-01 19:12:24 -0300

    se agregaron validaciones en el back al registro del formulario de usuarios

--- src/routes/users-routes.js
+++ src/routes/users-routes.js
@@ -19,33 +19,33 @@ const storage = multer.diskStorage({
 //Validaciones del registro de usuario
 const validacionesRegistro = [
     check('firstname')
-        .notEmpty().withMessage('Se debe completar el Nombre')
+        .notEmpty().withMessage('Se debe completar el Nombre').bail()
         .isAlpha().withMessage('Se deben ingresar caracteres alfabéticos'),
     check('lastname')
-        .notEmpty().withMessage('Se debe completar el Apellido')
+        .notEmpty().withMessage('Se debe completar el Apellido').bail()
         .isAlpha().withMessage('Se deben ingresar caracteres alfabéticos'),
     check('doc_type')
         .notEmpty().withMessage('Se debe completar el Tipo de Documento'),
     check('nroDocumento')
-        .notEmpty().withMessage('Se debe completar el Número de Documento')
+        .notEmpty().withMessage('Se debe completar el Número de Documento').bail()
         .isInt().withMessage('Se deben ingresar caracteres numéricos'),
     check('birth_date')
-        .notEmpty().withMessage('Se debe completar la fecha de cumpleaños')
+        .notEmpty().withMessage('Se debe completar la fecha de cumpleaños').bail()
         .isDate().withMessage('Debe ser una fecha válida'),
     check('gender')
         .notEmpty().withMessage('Se debe elegir un género'),
     check('email')
-        .notEmpty().withMessage('Se debe ingresar el mail de contacto')
+        .notEmpty().withMessage('Se debe ingresar el mail de contacto').bail()
         .isEmail().withMessage('El mail debe tener un formato válido'),
     check('reEmail')
-        .notEmpty().withMessage('Se debe ingresar el mail de contacto')
+        .notEmpty().withMessage('Se debe ingresar el mail de contacto').bail()
         .isEmail().withMessage('El mail debe tener un formato válido'),
     check('password')
-        .notEmpty().withMessage('Se debe ingresar una password')
+        .notEmpty().withMessage('Se debe ingresar una password').bail()
         .isLength({min:8}).withMessage('La contraseña debe ser mínimo de 8 caracteres'),
     check('term')
         .notEmpty().withMessage('Se deben aceptar los términos y condiciones'),
-    
+     
 ];
 
 //Ejecución de Multer
