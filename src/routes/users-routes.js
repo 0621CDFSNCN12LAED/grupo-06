30bcd05ce7831420540f7cdcef7cf33e1dbecbaa
Gaston Isla Casares <gislacasares@gmail.com>
2021-12-01 00:04:34 -0300

    se agregaron validaciones en el back al registro del formulario de usuarios

--- src/routes/users-routes.js
+++ src/routes/users-routes.js
@@ -2,8 +2,7 @@ const express = require("express");
 const router = express.Router();
 const multer = require("multer");
 const usersController = require("../controllers/usersController");
-
-
+const {check} = require('express-validator');
 
 //Creacion de storage que ataja el endpoint archivos del formulario registro
 const storage = multer.diskStorage({
@@ -17,6 +16,38 @@ const storage = multer.diskStorage({
     },
 });
 
+//Validaciones del registro de usuario
+const validacionesRegistro = [
+    check('firstname')
+        .notEmpty().withMessage('Se debe completar el Nombre')
+        .isAlpha().withMessage('Se deben ingresar caracteres alfabéticos'),
+    check('lastname')
+        .notEmpty().withMessage('Se debe completar el Apellido')
+        .isAlpha().withMessage('Se deben ingresar caracteres alfabéticos'),
+    check('doc_type')
+        .notEmpty().withMessage('Se debe completar el Tipo de Documento'),
+    check('nroDocumento')
+        .notEmpty().withMessage('Se debe completar el Número de Documento')
+        .isInt().withMessage('Se deben ingresar caracteres numéricos'),
+    check('birth_date')
+        .notEmpty().withMessage('Se debe completar la fecha de cumpleaños')
+        .isDate().withMessage('Debe ser una fecha válida'),
+    check('gender')
+        .notEmpty().withMessage('Se debe elegir un género'),
+    check('email')
+        .notEmpty().withMessage('Se debe ingresar el mail de contacto')
+        .isEmail().withMessage('El mail debe tener un formato válido'),
+    check('reEmail')
+        .notEmpty().withMessage('Se debe ingresar el mail de contacto')
+        .isEmail().withMessage('El mail debe tener un formato válido'),
+    check('password')
+        .notEmpty().withMessage('Se debe ingresar una password')
+        .isLength({min:8}).withMessage('La contraseña debe ser mínimo de 8 caracteres'),
+    check('term')
+        .notEmpty().withMessage('Se deben aceptar los términos y condiciones'),
+    
+];
+
 //Ejecución de Multer
 const upload = multer({ storage: storage });
 
@@ -25,7 +56,8 @@ router.get("/perfil-usuario", usersController.perfilUsuario);
 router.get("/ingresar", usersController.showLogin);
 router.post("/login", usersController.login);
 router.get("/registro", usersController.registro);
-router.post("/registro",
+router.post("/registro", 
+    validacionesRegistro, //Validaciones en los campos de registro de Usuario
     upload.single("imagenPerfil"),
     usersController.crearPaciente);
 
