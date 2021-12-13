5f69ff9eb4f13d62e8ab003ed1cb7cedd5c125fc
Gaston Isla Casares <gislacasares@gmail.com>
2021-12-13 10:46:22 -0300

    api de categorias

--- src/controllers/api/api-categorias-controller.js
+++ src/controllers/api/api-categorias-controller.js
@@ -4,9 +4,41 @@ const categoriaService = require("../../services/categoria-service");
 const controller = {
     listadoCategorias: async(req, res) => {
         const categorias = await categoriaService.list();
+        let total_categorias = 0;
 
-        res.json(categorias);
-    },
+        //Creo el array de objetos de estudios agregandole la url de detalle         
+        categorias.map((categoria) => {
+            total_categorias += 1;
+            delete categoria.dataValues.id;
+            delete categoria.dataValues.estado;
+            delete categoria.dataValues.fecha_creacion;
+            delete categoria.dataValues.fecha_modificacion;
+        });
+ 
+        if(categorias){
+            
+            res.json({
+                meta: {
+                    status: 200,
+                    url: 'api/categorias',
+                    descripcion: 'listado de todos las categorias disponibles y activas'
+                },
+                data: {
+                    categorias: categorias,
+                    count: total_categorias,
+                },
+            });
+
+        } else {
+            res.json({
+                meta: {
+                    status: 503,                    
+                    url: 'api/categorias',
+                    descripcion: 'error'
+                },
+                data: 'no data'
+        });
+    }},
    
 };
 
--- src/controllers/api/mainController.js
+++ src/controllers/api/mainController.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const estudiosController = require("./api-estudios-controller");
 const pacientesController = require("./api-pacientes-controller");
+const categoriasController = require("./api-categorias-controller");
+
 
 //Rutas correspondientes a Estudios
 router.use("/estudios", estudiosController.listadoEstudios);
@@ -11,5 +13,8 @@ router.use("/estudio/:id", estudiosController.estudioDetalle);
 //Rutas correspondientes a Pacientes
 router.use("/pacientes", pacientesController.listadoPacientes);
 router.use("/paciente/:id", pacientesController.pacienteDetalle);
+
+//Rutas correspondientes a Categorías
+router.use("/categorias", categoriasController.listadoCategorias);
 
 module.exports = router;
