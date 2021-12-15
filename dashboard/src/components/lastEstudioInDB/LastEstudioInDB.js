223f37f7525b267ccc264fde07433f9f47023f9b
Gaston Isla Casares <gislacasares@gmail.com>
2021-12-15 18:46:17 -0300

    cambios en ultimo estudio creado

--- dashboard/src/components/lastEstudioInDB/LastEstudioInDB.js
+++ dashboard/src/components/lastEstudioInDB/LastEstudioInDB.js
@@ -24,7 +24,7 @@ export default class LastEstudioInDb extends Component{
                             {<img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: 40 +'rem'}} src={'http://localhost:3030/img/estudios_imagenes/' + this.state.estudio.img} alt="{this.state.estudio.titulo}"/>}
                         </div>
                         <p>{this.state.estudio.descripcion}</p>
-                        <a className="btn btn-danger" target="_blank" rel="nofollow" href="/">Ver más detalle</a>
+                        <a className="btn btn-danger" target="_blank" rel="nofollow" href={'http://localhost:3030/api/estudio/' + this.state.estudio.id}>Ver más detalle</a>
                     </div>
                 </div>
             </div>
